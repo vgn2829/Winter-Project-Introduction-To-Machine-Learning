d11d8c1078a2b6a8435fb9593b9ad913165071ec
YashGarg2005 <163020194+YashGarg2005@users.noreply.github.com>
2024-12-11 15:26:26 +0530

    Delete Assignment1_Yash_Garg_231185 directory

--- Assignment1_Yash_Garg_231185/Q7.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib as mt
-import matplotlib.pyplot as plt
-plt.hist(np.random.randn(100), bins = 20)
-plt.grid()
-plt.xlabel("Values")
-plt.ylabel("Frequency")
-plt.title("Histogram of Random Values")
-plt.show()
--- Assignment1_Yash_Garg_231185/Q8.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-arr1=np.random.randint(1,100,(3,3))
-print(arr1)
-print('*'*15)
-print(arr1[0:3]) #start:stop
